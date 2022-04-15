6fd9c66ddb2cbb4e8ef508cb41173c5923f3a0b4
Beserrovsky <felipebeserra25@gmail.com>
2022-04-15 12:29:28 -0300

    Import ep0 code into ep1

---
+++ EP1/ep1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main() { 
+    int seed, attempts, pass, guess;
+    
+    printf("Bem vinda(o) ao Numle\n");
+
+    printf("Digite a semente para sortear a senha (0 a 10000): ");
+    scanf("%d", &seed);
+    pass = ((8121 * seed + 28411) % 134456) % 10;
+    printf("Quantidade de tentativas (1 a 10): ");
+    scanf("%d", &attempts);
+
+    while (attempts > 0) {
+        printf("Digite a tentativa (0 a 99999): ");
+        scanf("%d", &guess);
+        if(pass == guess) {
+            printf("Voce acertou! A senha eh de fato %d.", pass);
+            break;
+        }
+        attempts--;
+    }
+
+    if (attempts==0) {
+        printf("Voce perdeu! A senha era %d.", pass);
+    }
+    return 0;
+}
