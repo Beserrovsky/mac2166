6898d8f963e511151960407fb43dd8eb35262c29
Beserrovsky <felipebeserra25@gmail.com>
2022-04-17 21:58:41 -0300

    EP1: Same position check and translation done

--- EP1/ep1.c
+++ EP1/ep1.c
@@ -10,8 +10,16 @@
 #include <stdio.h>
 
 int main() { 
-    int seed, attempts, pass, guess;
-    
+    int seed, attempts, pass, guess, temp, i;
+
+    char *positions[] = {
+                            "Primeiro",
+                            "Segundo",
+                            "Terceiro",
+                            "Quarto",
+                            "Quinto"
+                        };
+
     printf("Bem vinda(o) ao Numle\n");
 
     printf("Digite a semente para sortear a senha (0 a 10000): ");
@@ -21,27 +29,43 @@ int main() {
     scanf("%d", &attempts);
 
     while (attempts > 0) {
+
         printf("Digite a tentativa (0 a 99999): ");
         scanf("%d", &guess);
   
-        /* Threat digits from left to rigth */
-        /* Always 5 digits */
-        /* Check if digit is at the right place */
-        /* Output in PT-BR (translate digit to language) */
-        /* Check if digit is at the wrong place */
-        /* Output in PT-BR (translate digit to language) */
-
-        /* If pass and guess are equal, end and congratulate*/
-        /* Else, attempts--*/
-
-        if(pass == guess) {
+        if (pass == guess) {
             printf("Voce acertou! A senha eh de fato %d.", pass);
             break;
         }
+         
+        i = 0;
+        temp = 10000;
+        while (temp >= 1) {
+
+            /* positions[i] -> PT-BR position */
+
+            /* ((guess / temp) % 10) -> guess value at that position */
+            
+            /* ((pass / temp) % 10) -> pass value at that position */
+            
+            if (((guess / temp) % 10) == (((pass / temp) % 10))) {
+
+                printf("%s digito certo!\n", positions[i]);
+            }
+
+            /* printf("%s digito em posicao incorreta.\n", positions[i]); */
+
+            i++;
+            if (temp == 1) temp--;
+            else temp /= 10;
+        }
+
+        /* TODO: Check if digit is at the wrong place */
+
         attempts--;
     }
 
-    if (attempts==0) {
+    if (attempts == 0) {
         printf("Voce perdeu! A senha era %d.", pass);
     }
     return 0;
