3b56040e375bba40dd0909b0d9273b2ebe751705
Beserrovsky <felipebeserra25@gmail.com>
2022-04-19 10:43:06 -0300

    EP1: Number decomposition and general IO

--- EP1/ep1.c
+++ EP1/ep1.c
@@ -1,10 +1,19 @@
+/***************************************************************/
+/**                                                           **/
+/**   Felipe Beserra de Oliveira                 13683702     **/
+/**   Exercício-Programa 1                                    **/
+/**   Professor: Yoshiharu Kohayakawa                         **/
+/**   Turma: 03                                               **/
+/**                                                           **/
+/***************************************************************/
+
 #include <stdio.h>
 
 int main() {
     
     int seed, attempts;
-    int pass, p1, p2, p3, p4, p5;
-    int guess, g1, g2, g3, g4, g5;
+    int pass, p0, p1, p2, p3, p4;
+    int guess, g0, g1, g2, g3, g4;
     int temp;
 
     printf("Bem vinda(o) ao Numle\n");
@@ -14,16 +23,30 @@ int main() {
     pass = ((8121 * seed + 28411) % 134456) % 100000;
     printf("Quantidade de tentativas (1 a 10): ");
     scanf("%d", &attempts);
-    
+
+    p0 = (pass / 10000) % 10;
+    p1 = (pass / 1000) % 10;
+    p2 = (pass / 100) % 10;
+    p3 = (pass / 10) % 10;
+    p4 = (pass / 1) % 10;
+
     while (attempts > 0) {
     
+        printf("Digite a tentativa (0 a 99999): ");
+        scanf("%d", &guess);
+
         if (guess == pass) {
             printf("Voce acertou! A senha eh de fato %d.", pass);
             break;
-        }
+        }}
 
-        temp = 10000;
-        
+        g0 = (guess / 10000) % 10;
+        g1 = (guess / 1000) % 10;
+        g2 = (guess / 100) % 10;
+        g3 = (guess / 10) % 10;
+        g4 = (guess) % 10;
+
+        /* TODO: Game logic */
 
         attempts--;
     }
